2c710d77e9598ea3f65a27265e7904006b0d516d
StephanPumpernik <stephan.pumpernik@gmx.net>
2025-03-20 18:12:24 +0100

    map skript hinzugefügt

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
-console.log("Hallo!");
+var map = L.map('map').setView([-45.345275, 170.826061], 13);
+        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            maxZoom: 19,
+            attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+        }).addTo(map);
+        var marker = L.marker([-45.345275, 170.826061]).addTo(map);
+        marker.bindPopup("<b>Moeraki Boulders</b><br>I am a popup.").openPopup();
