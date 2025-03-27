a314f32c6cad8d957d24502a6a6ab509aea04026
StephanPumpernik <stephan.pumpernik@gmx.net>
2025-03-27 15:38:41 +0100

    var durch let ersetzt

--- main.js
+++ main.js
@@ -4,13 +4,13 @@ skript für nz reise
 */
 
 // Karte initialisieren
-var map = L.map('map').setView([-45.345275, 170.826061], 13);
+let map = L.map('map').setView([-45.345275, 170.826061], 13);
 //Hintergrund definieren
         L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
             maxZoom: 19,
             attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
         }).addTo(map);
         //marker zeichnen
-        var marker = L.marker([-45.345275, 170.826061]).addTo(map);
+        let marker = L.marker([-45.345275, 170.826061]).addTo(map);
         //popup definieren und öffnen
         marker.bindPopup("<b>Moeraki Boulders</b><br>I am a popup.").openPopup();
