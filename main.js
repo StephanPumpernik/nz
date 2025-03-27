bc6ba2c57d5046a8f77aa2928b045830be751147
StephanPumpernik <stephan.pumpernik@gmx.net>
2025-03-27 16:30:01 +0100

    objekt erstellt

--- main.js
+++ main.js
@@ -2,22 +2,19 @@
 skript für nz reise
 */
 
-let lat = -45.345275;
-let lng = 170.826061;
-let zoom = 13;
 
 let stop = {
     nr: 7,
     titel: "Moeraki Boulders",
     user: "StephanPumpernik",
-    lat: lat,
-    lng: lng,
-    zoom: zoom,
+    lat: -45.345275,
+    lng: 170.826061,
+    zoom: 13,
 };
 
 
 // Karte initialisieren
-let map = L.map('map').setView([lat, lng], zoom);
+let map = L.map('map').setView([stop.lat, stop.lng], stop.zoom);
 
 //Hintergrund definieren
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -26,14 +23,14 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 }).addTo(map);
 
 //marker zeichnen
-let marker = L.marker([lat, lng]).addTo(map);
+let marker = L.marker([stop.lat, stop.lng]).addTo(map);
 
 //popup definieren und öffnen
 marker.bindPopup(`
            <h2> Moreaki Boulders </h2>
            <ul>
-            <li> Geogr. Breite: ${lat.toFixed(5)}° </>
-            <li> Geogr. Länge: ${lng.toFixed(5)}° </>
+            <li> Geogr. Breite: ${stop.lat.toFixed(5)}° </>
+            <li> Geogr. Länge: ${stop.lng.toFixed(5)}° </>
            </ul>
             `).openPopup();
 
