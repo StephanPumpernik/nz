c06b406860f59bb1e2b500040513f675453a8625
StephanPumpernik <stephan.pumpernik@gmx.net>
2025-03-27 15:47:39 +0100

    definiere koordinaten variablen definieren

--- main.js
+++ main.js
@@ -1,16 +1,17 @@
 /*
 skript für nz reise
-
 */
-
+let lat = -45.345275;
+let lng = 170.826061;
+let zoom = 13;
 // Karte initialisieren
-let map = L.map('map').setView([-45.345275, 170.826061], 13);
+let map = L.map('map').setView([lat, lng],zoom );
 //Hintergrund definieren
         L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
             maxZoom: 19,
             attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
         }).addTo(map);
         //marker zeichnen
-        let marker = L.marker([-45.345275, 170.826061]).addTo(map);
+        let marker = L.marker([lat, lng]).addTo(map);
         //popup definieren und öffnen
         marker.bindPopup("<b>Moeraki Boulders</b><br>I am a popup.").openPopup();
